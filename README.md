3ec227d5a4bfb0e73acad071be59aeed77e489b5
Michael Irwin <mikesir87@gmail.com>
2014-10-02 18:30:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Mojarra JSF Javascript Bug
+
+This repository provides a very basic web application to demonstrate a bug in the Mojarra JSF Javascript implementation
+
+## Details of the Bug
+
+When the JSF AJAX Javascript library discovers a form with an encoding type of **multipart/form-data**, it creates an iframe for all f:ajax requests.
+
+When the iframe is created (lazily done on the first f:ajax trigger), the form's tag is updated with a target attribute to point to the iframe.  This causes all form submits to be posted to the iframe, from which the results are pulled and the page is updated.
+
+The bug lies in that the target continues to point to the iframe.  This then causes all non-ajax form submissions to still go to the iframe, meaning successful form submissions are hidden, as well as all form validation errors are hidden, without the user knowing a thing (without looking at the server console).
+
+## Patching the Bug
+
+The patch is quite simple... return the form's target back to its original value after the form has been submitted.
+
+## To Run the Demo
+
+Simply checkout the code and run
+
+```
+mvn wildfly:run
+```
+
+A Wildfly instance will download, startup, and then deploy your app.  You can then access it at http://localhost:8080/demo.  From there, you'll have the option to view the bugged or patched demos.
+
---
+++ src/main/java/demo/DemoBean.java
@@ -0,0 +1,57 @@
+/*
+ * File created on Oct 2, 2014 
+ *
+ * Copyright (c) 2014 Virginia Polytechnic Institute and State University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package demo;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ * DESCRIBE THE TYPE HERE.
+ *
+ * @author Michael Irwin
+ * @author Chris Dunavant
+ */
+@Named
+@RequestScoped
+public class DemoBean {
+
+  private String name;
+
+  public String submit() {
+    System.out.println("Thanks!");
+    return "success";
+  }
+  
+  /**
+   * Gets the {@code name} property.
+   * @return property value
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Sets the {@code name} property.
+   * @param name the value to set
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+}
